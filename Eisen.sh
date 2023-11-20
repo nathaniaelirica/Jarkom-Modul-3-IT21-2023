df629cc1c01597a25a6f821eb2c84b5c264c7c9f
elvaraelvara <107627367+elvaraelvara@users.noreply.github.com>
2023-11-20 21:38:49 +0700

    Update Eisen.sh

--- Eisen.sh
+++ Eisen.sh
@@ -1,30 +1,56 @@
-echo nameserver 192.168.122.1 > /etc/resolv.conf
-
 apt-get update
-apt-get install apache2-utils -y
 apt-get install nginx -y
 apt-get install lynx -y
+apt-get install htop -y
+
+apt-get install apache2-utils -y
 
 service nginx start
+cp /etc/nginx/sites-available/default /etc/nginx/sites-available/lb_php
 
-echo '
-upstream worker {
-    least_conn;
-    server 10.74.4.6:8001;
-    server 10.74.4.5:8002;
-    server 10.74.4.4:8003;
+mkdir /etc/nginx/rahasisakita
+
+echo ' upstream worker {
+#    server 10.74.3.4;
+#    server 10.74.3.5;
+    server 10.74.3.6;
 }
 
 server {
     listen 80;
-    server_name riegel.canyon.it21.com www.riegel.canyon.it21.com;
+    server_name granz.channel.it21.com www.granz.channel.it21.com;
+
+    root /var/www/html;
+
+    index index.html index.htm index.nginx-debian.html;
+
+    server_name _;
 
     location / {
-        proxy_pass http://worker;
-    }
-}
-' > /etc/nginx/sites-available/laravel-worker
+    allow 10.74.3.69;
+    allow 10.74.3.70;
+    allow 10.74.4.167;
+    allow 10.74.4.168;
+    deny all;
+       proxy_pass http://worker;
 
-ln -s /etc/nginx/sites-available/laravel-worker /etc/nginx/sites-enabled/laravel-worker
+auth_basic "Restricted Content";
+auth_basic_user_file /etc/nginx/rahasisakita/htpasswd;
+    }
+
+ location ~ /its {
+        proxy_pass https://www.its.ac.id;
+        proxy_set_header Host www.its.ac.id;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+    }
+
+} ' > /etc/nginx/sites-available/lb_php
+
+ln -s /etc/nginx/sites-available/lb_php /etc/nginx/sites-enabled/
+rm /etc/nginx/sites-enabled/default
 
 service nginx restart
+
+htpasswd -c /etc/nginx/rahasisakita/htpasswd netics
